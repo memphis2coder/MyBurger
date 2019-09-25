48384bbd48f592645c1560ec532c0a7741afcff9
Antonio <Memphis2Coder@gmail.com>
2019-09-24 19:28:28 -0500

    added new component folders

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger- from BurgerBuilder.js</div>
+                <div>Build Controls - from BurgerBuilder.js</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
